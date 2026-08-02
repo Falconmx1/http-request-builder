ac81cef5f9444576e32f9e14b9c39285322df7c1
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-08-02 02:39:08 -0600

    Create __init__.py

---
+++ cli/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""
+Módulo CLI para manejo de argumentos y parsing.
+"""
+
+from cli.parser import create_parser, parse_headers
+
+__all__ = ['create_parser', 'parse_headers']
