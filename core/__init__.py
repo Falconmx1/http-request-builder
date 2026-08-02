19754c38f35cfe985c0f05d2f17f0d170a497c40
Mxfalconmx1 <mariofalcon030901@gmail.com>
2026-08-02 02:39:36 -0600

    Initialize core module for HTTP requests and responses

---
+++ core/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+Módulo core para manejo de peticiones y respuestas HTTP.
+"""
+
+from core.request import HTTPRequest
+from core.response import HTTPResponse
+
+__all__ = ['HTTPRequest', 'HTTPResponse']
